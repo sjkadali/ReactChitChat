a5561c07b53908150051cdd37cc86a7fd72f0523
sjkadali <skadalikat@gmail.com>
2020-07-07 16:57:36 -0400

    Updates to chat, index and new file users.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -19,6 +19,12 @@ const Chat = ({ location }) => {
 
         socket.emit('join', { name, room });
 
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+
     }, [ENDPOINT, location.search]);
 
     return (
